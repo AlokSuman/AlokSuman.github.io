521a28a9014f8cff5f120645a0f7e1db867fe508
Tech9logy <tech9logy@Tech9logys-Mac-mini.local>
2018-10-15 10:17:19 +0530

    first commit

---
+++ precache-manifest.11acad9cd5950de3e70c8d81ba736512.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "642fe5157b78b50453db",
+    "url": "/static/css/main.9b919fd9.chunk.css"
+  },
+  {
+    "revision": "07f7967c20bad8c0c08a2c53238fa2a1",
+    "url": "/static/media/banner2.07f7967c.jpg"
+  },
+  {
+    "revision": "506216e40f4ef0a5a58a",
+    "url": "/static/js/1.506216e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "642fe5157b78b50453db",
+    "url": "/static/js/main.642fe515.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "07f7967c20bad8c0c08a2c53238fa2a1",
+    "url": "/static/media/banner.07f7967c.jpg"
+  },
+  {
+    "revision": "07f7967c20bad8c0c08a2c53238fa2a1",
+    "url": "/static/media/banner1.07f7967c.jpg"
+  },
+  {
+    "revision": "506216e40f4ef0a5a58a",
+    "url": "/static/css/1.c40b2186.chunk.css"
+  },
+  {
+    "revision": "978c7a3b18b209ddc2d6989b95c75645",
+    "url": "/index.html"
+  }
+];
